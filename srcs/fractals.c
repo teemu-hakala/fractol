8fd2a044d52d17d950f2802e70e66f29490badbe
Teemu Hakala <thakala@c1r6p12.hive.fi>
2022-09-29 14:27:28 +0300

    quick fix to call scaler right

--- srcs/fractals.c
+++ srcs/fractals.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 12:41:52 by thakala           #+#    #+#             */
-/*   Updated: 2022/09/29 14:22:29 by thakala          ###   ########.fr       */
+/*   Updated: 2022/09/29 14:27:09 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,8 @@ void	mandelbrot(t_prg *pr)
 	{
 		while (px.col < WIN_HEIGHT)
 		{
-			p0 = (t_ldbl_pt){.row = scale(-1.12L, px.row, 1.12L), \
-				.col = scale(-2.00L, px.row, 0.47L)};
+			p0 = (t_ldbl_pt){.row = scale(-1.12L, WIN_HEIGHT, px.row, 1.12L), \
+				.col = scale(-2.00L, WIN_WIDTH, px.col, 0.47L)};
 			mandelbrot_plot(pr, &p0);
 			px.col++;
 		}
