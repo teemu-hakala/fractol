044317fab3e6ddee4bdc00924a3d135c9c31346c
Teemu Hakala <thakala@c1r6p12.hive.fi>
2022-09-29 16:32:52 +0300

    fractol.h(eader) norminette error: misalignment of function pointer typedef compared to struct typedefs

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 11:14:24 by thakala           #+#    #+#             */
-/*   Updated: 2022/09/29 16:31:14 by thakala          ###   ########.fr       */
+/*   Updated: 2022/09/29 16:31:59 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,7 +65,7 @@ typedef struct s_mouse			t_mse;
 typedef struct s_keyboard		t_key;
 typedef struct s_user_inputs	t_ipt;
 typedef struct s_program		t_prg;
-typedef void	(*t_ftype)(struct s_program *);
+typedef void					(*t_ftype)(struct s_program *);
 
 struct s_program
 {
