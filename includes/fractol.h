f0937bee78875e69865641121c4df7553832c18c
Teemu Hakala <thakala@c1r6p12.hive.fi>
2022-09-29 11:59:18 +0300

    detection of fractal_type

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,7 +6,58 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 11:14:24 by thakala           #+#    #+#             */
-/*   Updated: 2022/09/29 11:14:25 by thakala          ###   ########.fr       */
+/*   Updated: 2022/09/29 11:57:15 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FRACTOL_H
+# define FRACTOL_H
+# define STR_JULIA "julia"
+# define STR_MANDELBROT "mandelbrot"
+# define STR_BURNINGSHIP "burningship"
+
+# include <stdlib.h>
+
+typedef enum e_fractal_type
+{
+	FCT_JULIA,
+	FCT_MANDELBROT,
+	FCT_BURNINGSHIP
+}	t_type;
+
+typedef s_fractal
+{
+
+}	t_fct;
+
+typedef s_minilibx
+{
+
+}	t_mlx;
+
+typedef s_mouse
+{
+
+}	t_mse;
+
+typedef s_keyboard
+{
+
+}	t_key;
+
+typedef s_user_inputs
+{
+	t_mse	mse;
+	t_key	key;
+}	t_ipt;
+
+typedef struct s_program
+{
+	t_fct	*fct;
+	t_mlx	*mlx;
+	t_ipt	*inp;
+}	t_prg;
+
+void	usage(void);
+
+#endif
---
+++ srcs/fractol.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/29 11:17:26 by thakala           #+#    #+#             */
+/*   Updated: 2022/09/29 11:57:40 by thakala          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+int	fractal_type(t_fct *fractal, char *input)
+{
+	if (ft_strcmp(input, STR_JULIA) == EXIT_SUCCESS)
+		fractal->type = FCT_JULIA;
+	else if (ft_strcmp(input, STR_MANDELBROT) == EXIT_SUCCESS)
+		fractal->type = FCT_MANDELBROT;
+	else if (ft_strcmp(input, STR_BURNINGSHIP) == EXIT_SUCCESS)
+		fractal->type = FCT_BURNINGSHIP;
+	else
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
+
+int	main(int argc, char **argv)
+{
+	static t_fct	fct;
+	static t_mlx	mlx;
+	static t_ipt	ipt;
+	t_prg			prg;
+
+	if (argc != 2 || EXIT_SUCCESS != fractal_type(&fct, argv[1]))
+		usage();
+	prg = (t_prg){.fct = &fct, .mlx = &mlx, .ipt = &ipt};
+}
